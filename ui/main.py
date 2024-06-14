1c428a080e605c3bcb736e77b5b94f929b187c82
Dragon12112528 <2984723356@qq.com>
2024-06-14 22:50:43 +0800

    ui main

---
+++ ui/main.py
@@ -0,0 +1,107 @@
+import time
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import filedialog
+
+
+#.withdraw()  隐藏窗口
+#.deiconify() 显示窗口
+
+root = tk.Tk()
+reg = tk.Tk()
+reg.withdraw()
+cmp = tk.Tk()
+cmp.withdraw()
+aly = tk.Tk()
+aly.withdraw()
+def reg_w():
+    print("reg")
+    root.withdraw()
+    reg.deiconify()
+def cmp_w():
+    print("cmp")
+    root.withdraw()
+    cmp.deiconify()
+
+def aly_w():
+    print("aly")
+    root.withdraw()
+    aly.deiconify()
+
+
+def main():
+    print("start")
+    # 一个主界面 有三个按钮
+
+    # Create the main window
+    root.title("main")
+    # Create a button widget
+    button_reg = tk.Button(root, text="reg", command=reg_w)
+    button_cmp = tk.Button(root, text="cmp", command=cmp_w)
+    button_aly = tk.Button(root, text="aly", command=cmp_w)
+
+    # Pack the button into the main window
+    button_reg.pack(pady=10)
+    button_cmp.pack(pady=10)
+    button_aly.pack(pady=10)
+    # max
+    root.state('zoomed')
+    # Start the Tkinter event loop
+    root.mainloop()
+
+
+main()
+
+# 选文件
+# def select_file():
+#     root = tk.Tk()
+#
+#     file_path = filedialog.askopenfilename()
+#     return file_path
+#
+# selected_file = select_file()
+# print("选择的文件路径：", selected_file)
+# 功能切换
+# def say_hello():
+#     name = entry.get()
+#     label.config(text=f"Hello, {name}!")
+#
+# def closeWindow():
+#     root.withdraw()
+#     time.sleep(3)
+#     root.deiconify()
+#
+# # Create the main window
+# root = tk.Tk()
+# root.title("Tkinter Hello World")
+#
+# # Create a label widget
+# label = tk.Label(root, text="Welcome to Tkinter!")
+#
+# # Pack the label into the main window
+# label.pack(pady=10)
+#
+# # Create a button widget
+# button = tk.Button(root, text="Say Hello", command=closeWindow)
+#
+# # Pack the button into the main window
+# button.pack(pady=10)
+#
+# # Create an entry widget
+# entry = tk.Entry(root)
+#
+# # Pack the entry widget into the main window
+# entry.pack(pady=10)
+#
+# # Load an image
+# image = ImageTk.PhotoImage(Image.open("image/sanjiao.jpg"))
+#
+# # Create a label to display the image
+# image_label = tk.Label(root, image=image)
+# image_label.pack(pady=10)
+#
+#
+# # max
+# root.state('zoomed')
+# # Start the Tkinter event loop
+# root.mainloop()
