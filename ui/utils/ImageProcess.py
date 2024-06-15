8a7b3c5824637b8421c71d082cf1f36da5fa3821
cswccc <14311883+cswccc@user.noreply.gitee.com>
2024-06-15 18:15:34 +0800

    fix bug

--- ui/utils/ImageProcess.py
+++ ui/utils/ImageProcess.py
@@ -19,7 +19,7 @@ def checkImageSize(img, max_w, max_h):
 def imageRead(img_path):
     img = cv.imread(img_path)
 
-    img = checkImageSize(img, 800, 800)
+    # img = checkImageSize(img, 800, 800)
 
     return img
 
